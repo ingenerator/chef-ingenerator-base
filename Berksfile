7e6cf02182f138d714a0aa01be6be808a29d0700
Andrew Coulton <andrew@ingenerator.com>
2016-09-07 20:39:20 +0100

    Extract ingenerator environment helpers to helpers package
    For easier reuse in other cookbooks without having to have all the
    cookbooks depend on ingenerator-base

--- Berksfile
+++ Berksfile
@@ -1,2 +1,4 @@
 source 'https://supermarket.chef.io'
 metadata
+
+cookbook 'ingenerator-helpers', github: 'ingenerator/chef-ingenerator-helpers', branch: '1.x'
--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -13,36 +13,16 @@ module Ingenerator
           raise ArgumentError.new('The SSH port configured in node[\'ssh\'][\'host_port\'] must be an integer')
         end
 
-
-        if is_environment?(:localdev) || is_environment?(:buildslave)
+        if node.is_environment?(:localdev, :buildslave)
           22
         else
           node['ssh']['host_port']
         end
       end
 
-      # Test if this is the specified environment
-      def is_environment?(env)
-        node_environment === env.to_sym
-      end
-
-      # Test if this is not the specified environment
-      def not_environment?(env)
-        node_environment != env.to_sym
-      end
-
-      # Get the current node environment
-      def node_environment
-        if node['ingenerator'] && node['ingenerator']['node_environment']
-          node['ingenerator']['node_environment'].to_sym
-        else
-          :production
-        end
-      end
     end
   end
 end
 
 # Make the helpers available in all recipes
 Chef::Recipe.send(:include, Ingenerator::Base::Helpers)
-Chef::Node.send(:include, Ingenerator::Base::Helpers)
