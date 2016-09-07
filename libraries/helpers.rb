ae2a701610828b0ae0358bea4c4408e48e4a9050
Andrew Coulton <andrew@ingenerator.com>
2016-09-07 11:59:48 +0100

    Make the node_environment helpers tolerant of strings and symbols
    Attribute values that come from note attributes and Vagrantfiles are
    passed by JSON and so become strings along the way. This then causes
    issues because 'production' != :production. Internally make the
    helper pick this up and handle it properly.

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -23,18 +23,18 @@ module Ingenerator
 
       # Test if this is the specified environment
       def is_environment?(env)
-        node_environment === env
+        node_environment === env.to_sym
       end
 
       # Test if this is not the specified environment
       def not_environment?(env)
-        node_environment != env
+        node_environment != env.to_sym
       end
 
       # Get the current node environment
       def node_environment
         if node['ingenerator'] && node['ingenerator']['node_environment']
-          node['ingenerator']['node_environment']
+          node['ingenerator']['node_environment'].to_sym
         else
           :production
         end
--- spec/libaries/helpers_spec.rb
+++ spec/libaries/helpers_spec.rb
@@ -56,47 +56,78 @@ describe Ingenerator::Base::Helpers do
 
   end
 
-  describe 'node_environment' do
-    it 'returns :production if nothing configured' do
-      allow(my_recipe).to receive(:node).and_return({})
-      expect(my_recipe.node_environment).to eq(:production)
+  shared_examples 'it handles the node_environment as expected' do | is_environment, not_environment |
+
+    describe 'node_environment' do
+      it "is :#{is_environment}" do
+        expect(my_recipe.node_environment).to be(is_environment)
+      end
     end
 
-    it 'returns configured environment' do
+    describe 'is_environment?' do
+      it "matches :#{is_environment}" do
+        expect(my_recipe.is_environment?(is_environment)).to be(true)
+      end
+
+      it "matches '#{is_environment}'" do
+        expect(my_recipe.is_environment?("#{is_environment}")).to be(true)
+      end
+
+      it "does not match :#{not_environment}" do
+        expect(my_recipe.is_environment?(not_environment)).to be(false)
+      end
+
+      it "does not match '#{not_environment}'" do
+        expect(my_recipe.is_environment?("#{not_environment}")).to be(false)
+      end
+    end
+
+    describe 'not_environment?' do
+      it "matches :#{not_environment}" do
+        expect(my_recipe.not_environment?(not_environment)).to be(true)
+      end
+
+      it "matches '#{not_environment}'" do
+        expect(my_recipe.not_environment?("#{not_environment}")).to be(true)
+      end
+
+      it "does not match :#{is_environment}" do
+        expect(my_recipe.not_environment?(is_environment)).to be(false)
+      end
+
+      it "does not match '#{is_environment}'" do
+        expect(my_recipe.not_environment?("#{is_environment}")).to be(false)
+      end
+
+    end
+  end
+
+  context 'when no node_environment is configured' do
+    before(:each) do
+      allow(my_recipe).to receive(:node).and_return({})
+    end
+
+    include_examples 'it handles the node_environment as expected', :production, :localdev
+  end
+
+  context "when node_environment is configured as (string) 'buildslave'" do
+    before(:each) do
+      allow(my_recipe).to receive(:node).and_return({
+        'ingenerator' => {'node_environment' => 'buildslave'}
+      })
+    end
+
+    include_examples 'it handles the node_environment as expected', :buildslave, :production
+  end
+
+  context 'when node_environment is configured as (symbol) :localdev' do
+    before(:each) do
       allow(my_recipe).to receive(:node).and_return({
         'ingenerator' => {'node_environment' => :localdev}
       })
-      expect(my_recipe.node_environment).to eq(:localdev)
     end
+
+    include_examples 'it handles the node_environment as expected', :localdev, :production
   end
-
-  describe 'is_environment?' do
-    before (:each) do
-      allow(my_recipe).to receive(:node).and_return({})
-    end
-
-    it 'is true if environment matches' do
-      expect(my_recipe.is_environment?(:production)).to be(true)
-    end
-
-    it 'is false if environment does not match' do
-      expect(my_recipe.is_environment?(:localdev)).to be(false)
-    end
-  end
-
-  describe 'not_environment?' do
-    before (:each) do
-      allow(my_recipe).to receive(:node).and_return({})
-    end
-
-    it 'is false if environment matches' do
-      expect(my_recipe.not_environment?(:production)).to be(false)
-    end
-
-    it 'is true if environment does not match' do
-      expect(my_recipe.not_environment?(:localdev)).to be(true)
-    end
-  end
-
 
 end
